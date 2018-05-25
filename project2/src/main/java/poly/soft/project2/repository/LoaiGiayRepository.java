6451307881d5b6863c4e04e76474f57e63b56b67
hlong232 <hlong232@gmail.com>
2018-05-25 20:49:36 +0700

    update get LoaiGiay

--- project2/src/main/java/poly/soft/project2/repository/LoaiGiayRepository.java
+++ project2/src/main/java/poly/soft/project2/repository/LoaiGiayRepository.java
@@ -1,9 +1,14 @@
 package poly.soft.project2.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import poly.soft.project2.entity.LoaiGiay;
 
 public interface LoaiGiayRepository extends JpaRepository<LoaiGiay, Integer> {
 
+	@Query("from LoaiGiay lg group by lg.tenLoai")
+	List<LoaiGiay> getLoaiGiay();
 }
