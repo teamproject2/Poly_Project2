77bdd938477205ac9581557ebcc83a9c66a2f16e
vu long <hlong232@gmail.com>
2018-06-09 23:58:59 +0700

    update get list products before transfer to cart

--- karl-shop/src/app/components/cart/cart.component.ts
+++ karl-shop/src/app/components/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductSelected } from '../../entity/product-selected-cart';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
+  _productInCart: ProductSelected[] = [];
   constructor() { }
 
   ngOnInit() {
+    if (sessionStorage.productInCart !== null) {
+      this._productInCart = JSON.parse(sessionStorage.productInCart);
+    }
+    console.log("In cart:" + JSON.stringify(this._productInCart));
   }
 
 }
--- karl-shop/src/app/components/product-detail/product-detail.component.ts
+++ karl-shop/src/app/components/product-detail/product-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DetailsService } from '../../services/details.service';
 import { DetailProducts } from '../../entity/detail-products';
 import { ActivatedRoute, Router } from '@angular/router';
-import {ProductSelected} from '../../entity/product-selected-cart';
+import { ProductSelected } from '../../entity/product-selected-cart';
 import { SizeProduct } from '../../entity/size-product';
 declare var jquery: any;
 declare var $: any;
@@ -18,14 +18,20 @@ export class ProductDetailComponent implements OnInit {
   _detailProduct: DetailProducts;
   _hinhSanPham: string[] = [];
   id: number;
-  productSelect: ProductSelected = {idSP:1,sizeSP:""};
+  productSelect: ProductSelected = { idSP: 1, sizeSP: "" };
+  _productInCart: ProductSelected[] = [];
+  _productInCartOldLength: number;
 
   constructor(private detailService: DetailsService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.id = this.route.snapshot.params['id'];
     this.getDetailProduct(this.id);
-    // console.log(this.id);
+    if (sessionStorage.productInCart !== null) {
+      this._productInCart = JSON.parse(sessionStorage.productInCart);
+      this._productInCartOldLength = this._productInCart.length;
+    }
+    console.log("1:" + JSON.stringify(this._productInCart));
   }
 
   getDetailProduct(id) {
@@ -47,7 +53,28 @@ export class ProductDetailComponent implements OnInit {
   selectSize(size: SizeProduct) {
     this.productSelect.idSP = this.id;
     this.productSelect.sizeSP = size.tenKichThuoc;
-    console.log(this.productSelect);
+    
+    let count = 0;
+    if(this._productInCart.length === 0){
+      this._productInCart.push(this.productSelect);
+    }else{
+      if(this._productInCart.length > this._productInCartOldLength){
+        this._productInCart.pop();
+      }
+      for (let i = 0; i < this._productInCart.length; i++) {
+        if(JSON.stringify(this._productInCart[i]) === JSON.stringify(this.productSelect)){
+          count++;
+        }
+      }
+      if(count === 0){
+        this._productInCart.push(this.productSelect);
+      }
+    }
+    console.log("2:" + JSON.stringify(this._productInCart));
   }
 
+  addToCart() {
+    sessionStorage.productInCart = JSON.stringify(this._productInCart);
+    this.router.navigate(["home/cart"]);
+  }
 }
