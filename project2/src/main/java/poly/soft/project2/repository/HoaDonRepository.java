9a0b417a8c06062f14d7c8c64736e74a4dd1dda1
vu long <hlong232@gmail.com>
2018-06-22 23:30:38 +0700

    update HoaDonRepository

--- project2/src/main/java/poly/soft/project2/repository/HoaDonRepository.java
+++ project2/src/main/java/poly/soft/project2/repository/HoaDonRepository.java
@@ -1,6 +1,5 @@
 package poly.soft.project2.repository;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,6 +11,7 @@ public interface HoaDonRepository extends JpaRepository<HoaDon, Integer>{
 	
 	@Query(value="select hd.id, hd.ngay, kh.ten_khach_hang, nv.ten_nhan_vien, hd.trang_thai, hd.tong_tien, hd.thanh_toan, hd.ghi_chu from poly_project2.hoa_don hd " + 
 			"join poly_project2.khach_hang kh on hd.khach_hang_id = kh.id " + 
-			"join poly_project2.nhan_vien nv on hd.nhan_vien_id = nv.id ", nativeQuery = true)
+			"left outer join poly_project2.nhan_vien nv on hd.nhan_vien_id = nv.id " + 
+			"order by hd.id desc", nativeQuery = true)
 	public List<Object[]> getListHoaDon();
 }
